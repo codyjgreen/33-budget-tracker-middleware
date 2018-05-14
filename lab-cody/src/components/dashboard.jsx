88c43e181e0546c429a1323c9f0af8b3c25876ca
cody <codyjeromegreen@gmail.com>
2018-05-13 21:43:36 -0700

    initial commit

---
+++ lab-cody/src/components/dashboard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {
+    categoryCreate,
+    categoryUpdate,
+    categoryDestroy,
+} from '../actions/category-actions.jsx';
+
+import CategoryList from './category-list.jsx';
+import CategoryForm from './category-form.jsx';
+
+class Dashboard extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+           <h1>{this.props.addName}</h1>
+           <CategoryForm name='create' createCategory={this.props.createCategory} />
+           <CategoryList categories={this.props.categories}
+                        categoryUpdate={this.props.categoryUpdate}
+                        categoryDestroy={this.props.categoryDestroy}
+           />
+        </div>
+    }
+};
+
+const mapStateToProps = state => ({
+    appName: state.appName,
+    categories: state.categories
+});
+
+const mapDispatchToProps = (dispatch, getState) => {
+    return {
+        categoryCreate: (data) => dispatch(categoryCreate(data)),
+        categoryUpdate: (data) => dispatch(categoryUpdate(data)),
+        categoryDestroy: (data) => dispatch(categoryDestroy(data)),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
